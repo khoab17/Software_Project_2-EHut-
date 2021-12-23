9447d1bd591ca18044c5f50d78a75e44c49a250a
Born0 <bornoahmed2@gmail.com>
2021-12-23 20:49:12 +0600

    Removed Constrains From Customer

---
+++ EHut API/DAL/Migrations/202112231448164_RemovedConstrainsFromCustomer.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemovedConstrainsFromCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "NumberOfFamilyMemberAdult", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "NumberOfFamilyMemberAdult", c => c.Int(nullable: false));
+        }
+    }
+}
--- EHut API/DAL/Models/Customer.cs
+++ EHut API/DAL/Models/Customer.cs
@@ -14,26 +14,26 @@ namespace DAL.Models
 
 
         
-        [StringLength(50, ErrorMessage = "Can't be more than 50 character", MinimumLength = 1)]
+        //[StringLength(50, ErrorMessage = "Can't be more than 50 character", MinimumLength = 1)]
         public string Occupation { get; set; }
 
 
         
         [Range(0, 10, ErrorMessage = "Member can't be less than 0 or more than 10")]
-        public int NumberOfFamilyMemberAdult { get; set; }
+        public int? NumberOfFamilyMemberAdult { get; set; }
 
 
         
         [Range(0, 10, ErrorMessage = "Member can't be less than 0 or more than 10")]
-        public int NumberOfFamilyMemberChild { get; set; }
+        public int? NumberOfFamilyMemberChild { get; set; }
 
         
         [Range(0, 4, ErrorMessage = "Can't be less than 0 or more than 4")]
-        public int NumberOfDeliveryGrocery { get; set; }
+        public int? NumberOfDeliveryGrocery { get; set; }
 
         
         [Range(0, 8, ErrorMessage = "Member can't be less than 0 or more than 8")]
-        public int NumberOfDeliveryVegitables { get; set; }
+        public int? NumberOfDeliveryVegitables { get; set; }
 
         
         public string DeliveryDay { get; set; }
